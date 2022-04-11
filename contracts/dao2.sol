e1e6c17efef0996f4f7abd81f0dd19fe00b76fa3
Blessing Emah <beblessingemah@gmail.com>
2022-04-11 04:44:52 +0100

    Treasury Dao

--- contracts/dao2.sol
+++ contracts/dao2.sol
@@ -24,10 +24,11 @@ contract DAO2 {
         uint8 votes; //votes of the proposals
         bool executed;
         uint48 contributionEndTime;
-        address payable proposalAddress;
+        address proposalAuthor;
         uint256 receivedShares;
         uint256 requiredShares;
         string name;
+        mapping(address => bool) Voted;
     }
 
     uint32 constant contributionTime = 6 days;
@@ -43,7 +44,8 @@ contract DAO2 {
     mapping(address => bool) public isInvestor;
     mapping(address => uint256) public shares;
     mapping(uint256 => ProposalInfo) public proposals;
-    mapping(address => mapping(uint256 => bool)) public votes;
+
+    //mapping(address => mapping(uint256 => bool)) public votes;
 
     constructor() {
         admin = msg.sender;
@@ -73,8 +75,8 @@ contract DAO2 {
         uint256 derivedShares = amount * multiplier;
         isInvestor[msg.sender] = true;
         shares[msg.sender] = derivedShares;
-        //in the dao
-        availableFunds += msg.value;
+        availableFunds += msg.value;   //in the dao
+
     }
 
     function transferShare(uint256 amount, address to) external {
@@ -85,83 +87,65 @@ contract DAO2 {
         shares[to] += _amount;
     }
 
-    function createproposalId(
-        string memory name,
-        uint256 requiredShares,
-        address payable proposalAddress
-    ) external onlyInvestors {
+    function createproposalId(string memory name, uint256 requiredShares)
+        external
+        onlyInvestors
+    {
         ProposalInfo storage p = proposals[proposalId];
         p.name = name;
         p.requiredShares = requiredShares;
-        p.proposalAddress = address(this);
-
-
-        availableFunds -= amount;
+        p.proposalAuthor = msg.sender;
+        p.contributionEndTime = uint48(block.timestamp + contributionTime);
         proposalId++;
-
-        // uint8 votes; //votes of the proposals
-        // bool executed;
-        // uint48 contributionEndTime;
-        // address payable proposalAddress;
-        // uint256 requiredShares;
-        // string name;
     }
 
-     // require(availableFunds >= amount, "amount too big");
-        
-        // proposalId[nextProposalId] = ProposalInfo(
-        //     nextProposalId,
-        //     0, // number of votes
-        //     block.timestamp + voteTime, //enddate of the voting time
-        //     amount,
-        //     proposalAddress,
-        //     false,
-        //     name
-        // );
-
-    // function vote(uint256 Id) external onlyInvestors {
-    //     ProposalInfo storage proposal = proposalId[Id];
-    //     require(
-    //         votes[msg.sender][Id] == false,
-    //         "investor can only vote once for a proposal"
-    //     );
-    //     require(
-    //         block.timestamp <= proposal.contributionEndTime,
-    //         "can only vote until proposal end"
-    //     );
-    //     votes[msg.sender][Id] = true;
-    //     proposal.votes += shares[msg.sender];
-    // }
+    function vote(uint256 Id) external onlyInvestors {
+        ProposalInfo storage p = proposals[Id];
+        require(
+            block.timestamp > p.contributionEndTime,
+            "voting time has ended"
+        );
+        require(!p.Voted[msg.sender], "Investor has voted");
+        require(
+            p.receivedShares == p.requiredShares,
+            "Proposal shares has been met"
+        );
+        p.votes += 1;
+    }
 
     function executeProposal(uint256 Id) external onlyAdmin {
-        ProposalInfo storage proposal = proposals[Id];
+        ProposalInfo storage p = proposals[Id];
         require(
-            block.timestamp <= proposal.contributionEndTime,
+            block.timestamp <= p.contributionEndTime,
             "cannot execute a proposal before end date"
         );
         require(
-            proposal.executed == false,
-            "cannot execute a proposal already executed "
+            p.executed == false,
+            "cannot execute a proposal already executed"
         );
+        p.executed = true;
         //require((proposal.votes/ totalShares) * 100 >= approvers, "cannot execute proposal with votes below required approver amount");
-        _transferEther(proposal.amount, proposal.proposalAddress);
-        proposal.executed = true;
+        //transferEther(p.amount, p.proposalAddress);
     }
 
-    function withdrawEther(uint256 amount, address payable to)
-        external
-        onlyAdmin
-    {
-        _transferEther(amount, to);
-    }
+    // function withdrawEther(uint256 amount, address payable to)
+    //     external
+    //     onlyAdmin
+    // {
+    //     _transferEther(amount, to);
+    // }
 
-    function _transferEther(uint256 amount, address payable to) internal {
-        require(amount <= availableFunds, "not enough available funds");
-        availableFunds -= amount;
-        to.transfer(amount);
-    }
-
-    // function viewStruct(uint id) public view returns(ProposalInfo memory){
-    //     return proposalId[id];
+    // function _transferEther(uint256 amount, address payable to) internal {
+    //     require(amount <= availableFunds, "not enough available funds");
+    //     availableFunds -= amount;
+    //     to.transfer(amount);
     // }
 }
+
+    // the contributiontime has not ended.
+    //  check if their shares is greater than the minimum shares.
+
+    // require(
+    //     votes[msg.sender][Id] == false,
+    //     "investor can only vote once for a proposal"
+    // );
